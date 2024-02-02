9a6b948e52983d54f0a1fc16179339dfd0b30986
ronneylogs <ronneylok@gmail.com>
2024-02-02 13:55:16 -0800

    added tailwind css and daisyUI

--- src/screen/MainPage.js
+++ src/screen/MainPage.js
@@ -1,9 +1,9 @@
 const MainPage = () => {
-    return (
-        <div className="MainPage">
-            <h1>My First React App</h1>
-        </div>
-    )
-}
+  return (
+    <div className="MainPage">
+      <h1 className="text-white">My First React App</h1>
+    </div>
+  );
+};
 
 export default MainPage;
