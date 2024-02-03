6b2b777371a794b6acc77335cc6f575d5fe217db
ronneylogs <ronneylok@gmail.com>
2024-02-02 17:28:32 -0800

    made merge fixes

--- src/screen/HomePage.js
+++ src/screen/HomePage.js
@@ -10,12 +10,12 @@ const HomePage = () => {
   };
 
   return (
-    <div className="MainPage">
+    <div className="MainPage overscroll-none">
       <img
-        className="h-screen w-full relative"
+        className="h-screen w-full relative overscroll-none"
         src="./naturebackground.jpeg"
       ></img>
-      <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-between mt-10">
+      <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-between">
         <div className="flex flex-row justify-center align-center mt-[20vh]">
           <div className="flex flex-col justify-center align-center items-center w-4/5 p-5 md:p-10 border-4 border-black bg-white rounded-lg ">
             <h1 className="text-4xl md:text-9xl text-black font-bold ">
@@ -27,12 +27,12 @@ const HomePage = () => {
             ></Typewriter>
           </div>
         </div>
-        <div className="flex justify-center mb-[15vh]">
+        <div
+          className="flex justify-center mb-[15vh]"
+          onClick={moveToLandPage1}
+        >
           <div className="py-[3vh] px-[8%] border-4 border-black rounded-md shadow-lg bg-white">
-            <button
-              className="text-2xl text-black font-bold"
-              onClick={moveToLandPage1}
-            >
+            <button className="text-2xl text-black font-bold">
               Get Started
             </button>
           </div>
--- src/screen/subscreen/LandPage1.js
+++ src/screen/subscreen/LandPage1.js
@@ -10,24 +10,24 @@ const LandPage1 = () => {
   };
 
   return (
-    <div className="MainPage">
+    <div className="MainPage overscroll-none">
       <img
         className="h-screen w-full relative"
         src="./naturebackground.jpeg"
       ></img>
-      <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-between mt-10">
-        <div className="flex flex-row justify-center">
+      <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-between">
+        <div className="flex flex-row justify-center mt-[10vh]">
           <div className="w-[80vw] px-7 py-2  border-4 border-black rounded-md shadow-lg bg-white">
             <Typewriter text="Weelcome to Sproutful. This app is designed for you to practice mindfulness breathing exercises each day. As you progress, watch your very own mental tree grow. 😃" />
           </div>
         </div>
-        <img className="w-[50%] mx-auto" src="logo512.png"></img>
-        <div className="flex justify-center mb-[10vh]">
+        <img className="w-[50wh] mx-auto" src="tree4.png"></img>
+        <div
+          className="flex justify-center mb-[10vh]"
+          onClick={moveToMeditationPage}
+        >
           <div className="py-[3vh] px-[10%] border-4 border-black rounded-md shadow-lg bg-white">
-            <button
-              className="text-2xl text-black font-bold"
-              onClick={moveToMeditationPage}
-            >
+            <button className="text-2xl text-black font-bold">
               Start your journey now
             </button>
           </div>
