c358954810ae05b5d823b92b43ddd76378e95637
mathewtse <mathewtse@d207-023-191-044.wireless.sfu.ca>
2024-02-02 17:46:51 -0800

    updated watering page

--- src/screen/WateringPage.js
+++ src/screen/WateringPage.js
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import lottie from "lottie-web";
 
-import waterBackground from "../components/waterBackground";
 
 const WateringPage = () => {
   const navigate = useNavigate();
@@ -10,8 +10,23 @@ const WateringPage = () => {
     navigate("/");
   };
 
+  useEffect(() => {
+    const instance = lottie.loadAnimation({
+        animationData: require("./watering-animation.json"),
+        autoplay: true,
+        container: container.current,
+        loop: true,
+        renderer: "svg",    
+    })
+    // Cleanup function
+    return () => {
+        instance.destroy();
+    };
+  }, [])
+
   // add logic such that the tree image path changes depending on the user
-  const treeImagePath = "tree5.png"
+  const treeImagePath = "tree1.png"
+  const container = useRef(null);
 
   return (
     <div className="">
@@ -19,13 +34,28 @@ const WateringPage = () => {
         className="h-screen w-full relative"
         src="./wateringbackgroundimage.jpeg"
       ></img>
-      <div className="absolute bottom-40 right-80">
+
+<div className="absolute top-[200px] left-[100px] text-3xl font-bold text-blue-500 border-2 border-blue-500 p-4 ">
+        <h1>Congratulations!</h1>
+        <h1>We hope you feel refreshed</h1>
+        <h1>with every breath</h1>
+
+    </div>
+
+      {/* Position the Lottie animation */}
+      <div className="absolute bottom-[200px] right-[20px]"> {/* Adjust these values */}
+        <div ref={container} id="animation-container" className="w-64 h-64" style={{ transform: 'scaleX(-1)'}}></div>
+      </div>
+  
+      {/* Keep the tree image at its original position */}
+      <div className="absolute bottom-40 right-60">
         <div className="relative bottom-500 left-10">
-            <img src={treeImagePath} ></img>
+          <img src={treeImagePath} ></img>
         </div>
       </div>
     </div>
   );
+  
 };
 
 export default WateringPage;
