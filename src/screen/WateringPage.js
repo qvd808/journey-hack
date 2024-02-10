c59367e587150c8db25bfe1e28745838cc192a81
Quang Vinh Dang <dqvinh808@gmail.com>
2024-02-10 00:26:48 -0800

    Update UI for more responsive

--- src/screen/WateringPage.js
+++ src/screen/WateringPage.js
@@ -54,89 +54,64 @@ const WateringPage = () => {
   var str = "< Go back to meditation";
 
   return (
-    <div className="WateringPage overflow-hidden">
-      <img className="h-screen w-full relative" src={WaterBackground}></img>
-
-      <div className="absolute top-[5vh] flex flex-col w-full">
+    <div className="">
+      <img className="absolute h-screen w-full" src={WaterBackground}></img>
+      <div className="absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center md:relative md:flex-col">
         <div
-          className="self-start ml-10 font-bold text-black border-2 border-black rounded-lg shadow-xl p-4 bg-white text-sm md:text-md"
+          className="md:relative md:left-10 my-10 self-start font-bold text-black border-2 border-black rounded-lg shadow-xl p-4 bg-white text-sm md:text-md"
           onClick={moveToMeditationPage}
         >
           {str}
         </div>
-        <div className="grid grid-rows-2">
-          <div className="row-span-6 self-start">
-            {showAnimation && (
-              <div className="absolute flex flex-row w-full justify-start md:justify-center">
+        <div className="absolute md:relative md:flex md:flex-row-reverse md:justify-around md:w-3/4">
+          <div className="grid grid-cols-2 items-center">
+            {showAnimation ? (
+              <div className="relative left-20 flex flex-row w-full justify-start ">
                 {" "}
                 {/* Adjust these values */}
                 <div
                   ref={container}
                   id="animation-container "
-                  className="w-64 h-64 md:ml-[10%]"
-                  style={{ transform: "scaleX(1)" }}
+                  className="w-64 h-64 md:ml-[10%] overflow-hidden md:w-72 md:h-72"
+                  style={{ transform: "scaleX(1)", zIndex: 1 }}
                 ></div>
               </div>
-            )}
-
-            {showButton ? (
-              indexTree === 6 ? (
-                <div
-                  className="flex w-full justify-center md:ml-[10%]"
-                  onClick={growTree}
-                >
+            ) : (
+              <span
+                className="flex w-full justify-center md:ml-[10%]"
+                onClick={growTree}
+              >
+                {indexTree === 6 ? (
                   <button
                     className="font-bold text-white text-center border-2 border-black rounded-lg shadow-xl p-4 bg-rose-700 self-center mt-[20%] text-sm md:text-md"
                     onClick={growTree}
+                    style={{ display: showButton ? "flex" : "none" }}
                   >
                     Rebirth Tree
                   </button>
-                </div>
-              ) : (
-                <div
-                  className="flex w-full justify-center md:ml-[10%]"
-                  onClick={growTree}
-                >
+                ) : (
                   <button
                     className="font-bold text-white text-center border-2 border-black rounded-lg shadow-xl p-4  bg-blue-500 self-start mt-[20%] text-sm md:text-md "
                     onClick={growTree}
+                    style={{ display: showButton ? "flex" : "none" }}
                   >
                     Water Tree
                   </button>
-                </div>
-              )
-            ) : (
-              <div
-                className="invisible font-bold text-white border-2 border-black rounded-lg shadow-xl p-4 bg-blue-500 self-center mt-[20%] text-sm md:text-md"
-                onClick={growTree}
-              >
-                <button onClick={growTree}>{`Water Tree`}</button>
-              </div>
+                )}
+              </span>
             )}
-            <div className="grid grid-cols-2">
-              <div>
-                {" "}
-                <div className="hidden md:block row-span-1">
-                  <div className="font-bold text-black border-2 border-black rounded-lg shadow-xl p-4 bg-white self-center mx-[10%] mt-[5%] text-sm md:text-md md:mb-10">
-                    <h1>Congratulations!</h1>
-                    <h1>We hope you feel refreshed</h1>
-                    <h1>with every breath.</h1>
-                  </div>
-                </div>
-              </div>
-              <div>
-                {/* Keep the tree image at its original position */}
-                <div className="grid grid-rows-2 w-full justify-center">
-                  <img className="row-span-2 w-52" src={treeImagePath}></img>
-                  <p className="row-span-1 font-bold text-amber-800 text-center text-xl text-sm md:text-lg">
-                    {treeName[indexTree]}
-                  </p>
-                </div>
-              </div>
+            <div className="relative grid grid-rows-2 w-full justify-center ">
+              <img
+                className="row-span-2 w-56 md:w-72"
+                src={treeImagePath}
+              ></img>
+              <p className="row-span-1 font-bold text-amber-800 text-center text-xl md:text-lg">
+                {treeName[indexTree]}
+              </p>
             </div>
           </div>
-          <div className="block md:hidden row-span-1">
-            <div className="font-bold text-black border-2 border-black rounded-lg shadow-xl p-4 bg-white self-center mx-[10%] mt-[5%] text-sm md:text-md md:mb-10">
+          <div className="absolute left-0 right-0 bottom-[-60%] flex justify-center items-center md:relative">
+            <div className="font-bold text-black border-2 border-black rounded-lg shadow-xl p-4 bg-white self-center mx-[10%] mt-[5%] text-sm md:text-xl md:mb-10">
               <h1>Congratulations!</h1>
               <h1>We hope you feel refreshed</h1>
               <h1>with every breath.</h1>
